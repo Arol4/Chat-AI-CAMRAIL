a02963153a1f809c046ecd035719bdb811497ca5
Djeufack kafack berios arol <arolddjeufack@gmail.com>
2026-08-15 12:23:16 +0100

    refactor: remove profile picture handling and simplify avatar display

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import profilePic from '../../assets/photo-profil.jpeg'
 import './UserProfile.css'
 
 export default function UserProfile({ collapsed }) {
@@ -10,16 +9,7 @@ export default function UserProfile({ collapsed }) {
   return (
     <div className={`user-profile ${collapsed ? 'compact' : ''}`}>
       <div className="avatar-circle">
-        {!imgError ? (
-          <img
-            src={profilePic}
-            alt="Profil"
-            onError={() => setImgError(true)}
-            className="avatar-img"
-          />
-        ) : (
-          <span className="avatar-initials">{initials}</span>
-        )}
+        <span className="avatar-initials">{initials}</span>
       </div>
       {!collapsed && <span className="username">Djeufack Kafack</span>}
     </div>
